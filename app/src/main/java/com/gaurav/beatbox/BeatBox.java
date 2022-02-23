f6c89d71e4d7ad0e65be5c16ff8af8de7429d04c
Gaurav Singh <gauravs21k@gmail.com>
2022-02-24 02:17:12 +0530

    sounds are loaded through SoundPool.load()

--- app/src/main/java/com/gaurav/beatbox/BeatBox.java
+++ app/src/main/java/com/gaurav/beatbox/BeatBox.java
@@ -1,7 +1,9 @@
 package com.gaurav.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -11,11 +13,15 @@ import java.util.List;
 public class BeatBox {
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
+    private static  final int MAX_SOUNDS = 5;
+
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        mSoundPool = new SoundPool.Builder().setMaxStreams(MAX_SOUNDS).build();
         loadSounds();
     }
 
@@ -29,10 +35,21 @@ public class BeatBox {
         }
 
         for (String filename: soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioException){
+                Log.e(TAG, "could not load sound " + filename, ioException);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException{
+        AssetFileDescriptor assetFileDescriptor = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(assetFileDescriptor,1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
